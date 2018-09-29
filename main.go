f80c785680851296f3ae142bc034f65fa3c4829d
pjserol <pjserol@gmail.com>
2018-09-29 16:21:41 +1000

    Add merge sort

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	fmt.Println("--------------------")
 	fmt.Println()
 
-	 unsorted := []int{4, 5, 1, 2, 3}
+	unsorted := []int{4, 5, 1, 2, 3}
 
 	fmt.Println("-----Bubble Sort-----")
 	sortedBubbleSort, nbIterationBubbleSort := BubbleSort(unsorted)
@@ -22,11 +22,20 @@ func main() {
 	sortedInsertionSort, nbIterationInsertionSort := selectionSort(unsorted)
 	displayResultSort(unsorted, sortedInsertionSort, nbIterationInsertionSort)
 
+	listMerge := make([]int, len(unsorted))
+
+	for i := range unsorted {
+		listMerge[i] = unsorted[i]
+	}
+
+	fmt.Println("-----Merge Sort-----")
+	mergeSort(listMerge, 0, len(listMerge)-1)
+	displayResultSort(unsorted, listMerge, 0)
 }
 
 func displayResultSort(unsorted []int, sorted []int, numberOfIteration int) {
 	fmt.Println("Before : ", unsorted)
-	fmt.Println("After  : ", sorted) 
-	fmt.Println("Number of iteration  : ", numberOfIteration) 
+	fmt.Println("After  : ", sorted)
+	fmt.Println("Number of iteration  : ", numberOfIteration)
 	fmt.Println()
 }
